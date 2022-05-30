c3cb8bf35e936654f727b54012c71349795163ad
LoicMonard <monard@intechinfo.fr>
2022-05-30 22:47:24 +0200

    :sparkles: Add card component
    Create simple card placeholder component

---
+++ src/lib/Card.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let card = {}
+</script>
+
+<li class="card">
+  <h2>{card.title}</h2>
+  <p>{card.description}</p>
+</li>
+
+<style>
+  .card {
+    background: #fff;
+    border-radius: 4px;
+    box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+    padding: 1em;
+    margin: 1em;
+  }
+</style>
